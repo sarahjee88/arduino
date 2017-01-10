674105e94d916b9d10275fcbddf4a9e308900ad1
Sarah Jee <sarahjee@seoulchristian.org>
2017-01-10 11:34:31 +0900

    Set pinMode 12, OUTPUT to digitalWrite and delay

--- blink/blink.ino
+++ blink/blink.ino
@@ -1,12 +1,19 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(13, HIGH);
-  delay(1000);
-  digitalWrite(13, LOW);
+  digitalWrite(12, HIGH);
+  // turns on the light of pin13
+  delay(100);
+  // turnes on for 0.1 sec
+  digitalWrite(12, LOW);
+  // turns off the light of pin13
+  delay(100);
+  digitalWrite(12, HIGH);
+  delay(2000);
+  digitalWrite(12, LOW);
   delay(1000);
 }
