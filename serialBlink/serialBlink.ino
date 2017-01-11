4e9c6f2461ba988f462d2ff4c85ca647e878e1d2
Sarah Jee <sarahjee@seoulchristian.org>
2017-01-11 10:24:30 +0900

    Add commands to tell whether LED is on or off

--- serialBlink/serialBlink.ino
+++ serialBlink/serialBlink.ino
@@ -1,12 +1,15 @@
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   pinMode (13, OUTPUT);
  }
 
 void loop() {
   // put your main code here, to run repeatedly:
-digitalWrite (13, HIGH);
-delay(1000);
-digitalWrite (13, LOW);
-delay(1000);
+  digitalWrite (13, HIGH);
+  Serial.println("on");
+  delay(1000);
+  digitalWrite (13, LOW);
+  Serial.println("off");
+  delay(1000);
 }
