eff04f26aa84941fd03aa79c539595157c9a32fe
Sarah Jee <sarahjee@seoulchristian.org>
2017-01-11 10:36:05 +0900

    Add message that shows up when arduino boots up

--- serialBlink/serialBlink.ino
+++ serialBlink/serialBlink.ino
@@ -1,8 +1,9 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  Serial.println("It's so nice to see you again!");
   pinMode (13, OUTPUT);
- }
+}
 
 void loop() {
   // put your main code here, to run repeatedly:
