b6a28ccc51cbf17f4c4e5c19e20167421c782a5e
Sarah Jee <sarahjee@seoulchristian.org>
2017-01-12 10:43:24 +0900

    Set arduino to light on/off without delay

--- serialBlink/serialBlink.ino
+++ serialBlink/serialBlink.ino
@@ -1,18 +1,25 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  Serial.setTimeout(10);
   Serial.println("It's so nice to see you again!");
-  Serial.println("Send 'on' to turn on the LED.");
-  Serial.println("Send 'off' to turn off the LED.");
+  Serial.println("Send 'On' to turn on the LED.");
+  Serial.println("Send 'Off' to turn off the LED.");
   pinMode (13, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (Serial.readString() == "On") {
-    digitalWrite(13, HIGH);
-   }
-  else if (Serial.readString() == "Off") {
-    digitalWrite(13, LOW);
-   }
- }
+  if (Serial.available() > 0) {
+    String command;
+    command = Serial.readString();
+    if (command == "On") {
+      digitalWrite(13, HIGH);
+      Serial.println("light ON!");
+    }
+    else if (command == "Off") {
+      digitalWrite(13, LOW);
+      Serial.println("light OFF!");
+    }
+  }
+}
