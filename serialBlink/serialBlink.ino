b18ad67212336494747eafc86cdc38e06f136702
Sarah Jee <sarahjee@seoulchristian.org>
2017-01-13 10:14:20 +0900

    Add comments on the code

--- serialBlink/serialBlink.ino
+++ serialBlink/serialBlink.ino
@@ -1,25 +1,25 @@
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
-  Serial.setTimeout(10);
-  Serial.println("It's so nice to see you again!");
-  Serial.println("Send 'On' to turn on the LED.");
-  Serial.println("Send 'Off' to turn off the LED.");
-  pinMode (13, OUTPUT);
+  Serial.begin(9600);  // start the seiial and set the baud rate to 9600
+  Serial.setTimeout(10);  // set arduino to scan the buffer per 0.001 sec
+  Serial.println("It's so nice to see you again!");  // set arduino to print message
+  Serial.println("Send 'On' to turn on the LED.");  // set arduino to print message
+  Serial.println("Send 'Off' to turn off the LED.");  // set arduino to print message
+  pinMode (13, OUTPUT);  // set pin13 to output
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (Serial.available() > 0) {
-    String command;
-    command = Serial.readString();
-    if (command == "On") {
-      digitalWrite(13, HIGH);
-      Serial.println("light ON!");
+  if (Serial.available() > 0) { // check anything for serial buffer
+    String command; // creates string variable called command
+    command = Serial.readString(); // set arduino to read message that are sent
+    if (command == "On") { // set arduino to do command if "On" is typed
+      digitalWrite(13, HIGH); // turn the light on of pin13
+      Serial.println("light ON!"); // set arduino to send message
     }
-    else if (command == "Off") {
-      digitalWrite(13, LOW);
-      Serial.println("light OFF!");
+    else if (command == "Off") { // set ardiono to do command if "Off" is typed
+      digitalWrite(13, LOW); // turn the light off of pin13
+      Serial.println("light OFF!"); // set arduino to send message
     }
   }
 }
