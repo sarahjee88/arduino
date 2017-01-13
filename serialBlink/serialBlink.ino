bd73da01d3da366878167cf304a1ff20ea6ddaaf
Sarah Jee <sarahjee@seoulchristian.org>
2017-01-13 10:21:24 +0900

    Update the comments on the code

--- serialBlink/serialBlink.ino
+++ serialBlink/serialBlink.ino
@@ -1,7 +1,7 @@
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);  // start the seiial and set the baud rate to 9600
-  Serial.setTimeout(10);  // set arduino to scan the buffer per 0.001 sec
+  Serial.begin(9600);  // start the serial and set the baud rate to 9600
+  Serial.setTimeout(10);  // set arduino to scan the buffer every 0.001 sec
   Serial.println("It's so nice to see you again!");  // set arduino to print message
   Serial.println("Send 'On' to turn on the LED.");  // set arduino to print message
   Serial.println("Send 'Off' to turn off the LED.");  // set arduino to print message
@@ -10,9 +10,9 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (Serial.available() > 0) { // check anything for serial buffer
+  if (Serial.available() > 0) { // check if anything is in the serial buffer
     String command; // creates string variable called command
-    command = Serial.readString(); // set arduino to read message that are sent
+    command = Serial.readString(); // set arduino to read message of the value ogitf the command
     if (command == "On") { // set arduino to do command if "On" is typed
       digitalWrite(13, HIGH); // turn the light on of pin13
       Serial.println("light ON!"); // set arduino to send message
