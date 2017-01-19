1a29c39af3b2156f5312caa8e7f6e8f737664043
Sarah Jee <sarahjee@seoulchristian.org>
2017-01-19 10:37:18 +0900

    Add comments on the code

--- forFade/forFade.ino
+++ forFade/forFade.ino
@@ -1,17 +1,17 @@
 void setup() {
-  Serial.begin(9600);
-  pinMode(9, OUTPUT);
+  Serial.begin(9600); //set serial connection at baud rate 9600
+  pinMode(9, OUTPUT); //set pin9 to output
 }
 
 void loop() {
-  for (int lightValue = 0; lightValue <= 255; lightValue++) {
-    analogWrite(9, lightValue);
-    Serial.println(lightValue);
-    delay(10);
+  for (int lightValue = 0; lightValue <= 255; lightValue++) { //increases and decreases the lightvalue
+    analogWrite(9, lightValue); //set brightness of pin9
+    Serial.println(lightValue); //print lightvalue on the serial monitor
+    delay(10); //wait for every 10 milliseconds
   }
-  for (int lightValue = 255; lightValue >= 0; lightValue--) {
-    analogWrite(9, lightValue);
-    Serial.println(lightValue);
-    delay(10);
+  for (int lightValue = 255; lightValue >= 0; lightValue--) { //increases and decreases the lightvalue
+    analogWrite(9, lightValue); //set brightness of pin9
+    Serial.println(lightValue); //print lightvalue on the serial monitor
+    delay(10); //wait for every 10 milliseconds
   }
 }
--- ifFade/ifFade.ino
+++ ifFade/ifFade.ino
@@ -1,18 +1,18 @@
-int lightValue = 0;
-int delta = 1;
+int lightValue = 0; //set integer valuable lightvalue to 0
+int delta = 1; // set integer variable delta to 1
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(9, OUTPUT);
+  Serial.begin(9600); //set serial connection at baud rate 9600
+  pinMode(9, OUTPUT); //activate pin9 to output
 }
 
 void loop() {
-  lightValue = lightValue + delta;
-  if (lightValue <= 0 || lightValue >= 255) {
-    delta = -delta;
+  lightValue = lightValue + delta; //set lightvalue to sum of lightvalue plus delta
+  if (lightValue <= 0 || lightValue >= 255) { //set command to activate under certain condition
+    delta = -delta; //set delta to beome negative
   }
-  analogWrite(9, lightValue);
-  Serial.println(lightValue);
-  delay(10);
+  analogWrite(9, lightValue); //set the brightness of pin9
+  Serial.println(lightValue); //print lightvalue to serial monitor
+  delay(10); //wait for every ten milliseconds
 }
 
