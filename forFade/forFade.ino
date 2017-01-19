281fa895b9c84df957c82b1af79dc0c8151a3e14
Sarah Jee <sarahjee@seoulchristian.org>
2017-01-19 10:45:32 +0900

    Fix the comments on the code

--- forFade/forFade.ino
+++ forFade/forFade.ino
@@ -1,17 +1,17 @@
 void setup() {
-  Serial.begin(9600); //set serial connection at baud rate 9600
-  pinMode(9, OUTPUT); //set pin9 to output
+  Serial.begin(9600); // set serial connection at baud rate 9600
+  pinMode(9, OUTPUT); // set pin9 to output
 }
 
 void loop() {
-  for (int lightValue = 0; lightValue <= 255; lightValue++) { //increases and decreases the lightvalue
-    analogWrite(9, lightValue); //set brightness of pin9
-    Serial.println(lightValue); //print lightvalue on the serial monitor
-    delay(10); //wait for every 10 milliseconds
+  for (int lightValue = 0; lightValue <= 255; lightValue++) { // increases the lightvalue until 255
+    analogWrite(9, lightValue); // set brightness of pin9
+    Serial.println(lightValue); // print lightvalue on the serial monitor
+    delay(10); // wait for ten milliseconds
   }
-  for (int lightValue = 255; lightValue >= 0; lightValue--) { //increases and decreases the lightvalue
-    analogWrite(9, lightValue); //set brightness of pin9
-    Serial.println(lightValue); //print lightvalue on the serial monitor
-    delay(10); //wait for every 10 milliseconds
+  for (int lightValue = 255; lightValue >= 0; lightValue--) { // decreases the lightvalue until 0
+    analogWrite(9, lightValue); // set brightness of pin9
+    Serial.println(lightValue); // print lightvalue on the serial monitor
+    delay(10); // wait for ten milliseconds
   }
 }
