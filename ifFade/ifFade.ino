d9d676800052fc8fc0e2d54212575b2e5ccb389e
Sarah Jee <sarahjee@seoulchristian.org>
2017-01-19 10:21:36 +0900

    Set LED to fade light on/off, and print lighvalue

--- ifFade/ifFade.ino
+++ ifFade/ifFade.ino
@@ -2,10 +2,17 @@ int lightValue = 0;
 int delta = 1;
 
 void setup() {
-  pinMode(9, OUTPUT);
   Serial.begin(9600);
+  pinMode(9, OUTPUT);
 }
 
 void loop() {
-
+  lightValue = lightValue + delta;
+  if (lightValue <= 0 || lightValue >= 255) {
+    delta = -delta;
+  }
+  analogWrite(9, lightValue);
+  Serial.println(lightValue);
+  delay(10);
 }
+
